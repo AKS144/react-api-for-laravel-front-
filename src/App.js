9926056a641faf9b484d35807d0f2d0a05ab576a
AKS <abhi144k@gmail.com>
2022-11-09 14:28:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//import Student from './containers/students'
+import Student from './students'
+
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Student/>
+      </div>
+    );
+  }
+  }
 
 export default App;
